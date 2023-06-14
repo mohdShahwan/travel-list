9a99dd5c3b1fc767a2e7d2104e00c98fcd1982ae
mohdShahwan <mshahwan002@gmail.com>
2023-06-14 18:55:05 +0300

    Sorting items

--- src/App.js
+++ src/App.js
@@ -84,10 +84,22 @@ function Form({ onAddItems }) {
 }
 
 function PackageList({ items, onDeleteItems, onUpdateItems }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  else if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  else
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -96,6 +108,14 @@ function PackageList({ items, onDeleteItems, onUpdateItems }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -105,7 +125,7 @@ function Item({ item, onDeleteItems, onUpdateItems }) {
     <li>
       <input
         type="checkbox"
-        onChange={(e) => onUpdateItems(item.id)}
+        onChange={() => onUpdateItems(item.id)}
         checked={item.packed}
       />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
